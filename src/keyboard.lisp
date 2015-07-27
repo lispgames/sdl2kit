8ffbae572e2ae3733c94d431a5a208e82e0db2eb
Ryan Pavlik <rpavlik@gmail.com>
2015-07-27 17:37:49 -0500

    1 = down

--- src/keyboard.lisp
+++ src/keyboard.lisp
@@ -7,8 +7,8 @@
   (with-slots (keystate) tracker
     (when (and state (not repeat-p))
       (case state
-        (:keydown (setf (aref keystate (sdl2:scancode-value keysym)) 0))
-        (:keyup   (setf (aref keystate (sdl2:scancode-value keysym)) 1))))))
+        (:keydown (setf (aref keystate (sdl2:scancode-value keysym)) 1))
+        (:keyup   (setf (aref keystate (sdl2:scancode-value keysym)) 0))))))
 
 (defun keystate (tracker scancode)
   (let ((num (autowrap:enum-value 'sdl2-ffi:sdl-scancode scancode)))
